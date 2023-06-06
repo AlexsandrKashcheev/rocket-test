15540e3c03547e4adf707edd7baeebf934a0ede2
Alexander Kashcheev <kashcheev.sasha53@gmail.com>
2023-06-06 03:08:48 +0300

    сделал функционал для слайдера

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@ import './reset.css'
 import './styles/main.scss';
 
 
+let position = 0;
+let count = 1;
+const slidesToShow = 1;
+const slidesToScroll = 1;
+const checkUpContainer = document.querySelector('.check-up__container');
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const items = document.querySelectorAll('.slider-item');
+const itemsCount = items.length;
+const btnLeft = document.querySelector('.arrow-left');
+const btnRight = document.querySelector('.arrow-right');
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
+const currentItem = document.querySelector('.current');
+const quantityItem = document.querySelector('.quantity');
+
+const setCurrentPosition = () => {
+    currentItem.innerHTML = `${count}`;
+    quantityItem.innerHTML = `${itemsCount}`;
+}
+
+setCurrentPosition();
+
 const closeMenu = document.getElementById('closeMenu');
 const menu = document.getElementById('menu');
 const burgerMenu = document.getElementById('burger');
@@ -17,3 +40,36 @@ closeMenu.addEventListener('click', () => {
     burgerMenu.style.display = 'block';
 });
 
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`;
+});
+
+
+btnRight.addEventListener('click', () => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    count++;
+    setCurrentPosition();
+    setPosition();
+    checkBtns();
+});
+
+btnLeft.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / itemWidth;
+    position += itemsLeft >= slidesToShow ? movePosition : itemsLeft * itemWidth;
+    count -= slidesToScroll;
+    setCurrentPosition();
+    setPosition();
+    checkBtns();
+});
+
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`;
+}
+
+const checkBtns = () => {
+    btnLeft.disabled = position === 0;
+    btnRight.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+}
+
+checkBtns();
